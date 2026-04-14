5045474919b5c33b26e4ad5e47d132d185ace74f
Helen Acero <hepacest@gmail.com>
2026-04-14 18:10:18 -0500

    The link path of the subproject is adjusted.

---
+++ app/markdown-editor/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function MarkdownEditor() {
+  redirect('https://markdown-editor-iota-eosin.vercel.app');
+}
--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/markdown-editor',
-        destination: 'https://markdown-editor-iota-eosin.vercel.app',
-      },
-      {
-        source: '/markdown-editor/:path*',
-        destination: 'https://markdown-editor-iota-eosin.vercel.app/:path*',
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
