a0478ba02ae683db5ce87ff1b793bb08788f9b32
Helen Acero <hepacest@gmail.com>
2026-04-14 16:36:58 -0500

    The vercel.json file is removed as it is no longer needed, and the file is edited to redirect to the subproject.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/proyecto-b',
+        destination: 'https://proyecto-b.vercel.app',
+      },
+      {
+        source: '/proyecto-b/:path*',
+        destination: 'https://proyecto-b.vercel.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
